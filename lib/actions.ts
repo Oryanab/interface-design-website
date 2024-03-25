78b26da7094755e909e23c2a08ca4ba14014564b
Oryan Abergel <86202122+Oryanab@users.noreply.github.com>
2024-03-26 01:32:43 +0200

    update grafbase

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,13 +1,18 @@
 import { GraphQLClient } from "graphql-request";
 
-const isProduction = process.env.NODE_ENV === 'production'
-const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GRAPHQL_API_URL
+const isProduction = process.env.NODE_ENV === "production";
+const apiUrl = isProduction
+  ? process.env.GRAPH_BASE_API_ENDPOINT || ""
+  : "http://127.0.0.1:4000/graphql";
+const apiKey = isProduction ? process.env.GRAPH_BASE_API_KEY || "" : "letmein";
+const serverUrl = isProduction
+  ? process.env.NEXT_PUBLIC_SERVER_URL
+  : "http://localhost:3000";
 
-const client = new GraphQLClient(process.)
+const client = new GraphQLClient(apiUrl);
 
 const makeGraphQLRequest = async (query: string, variables = {}) => {
   try {
-
   } catch (error) {
     throw error;
   }
